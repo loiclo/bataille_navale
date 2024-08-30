c48d4dbbd2083b911a1eafcaec937686e1f0923e
loiclo <lecinloic07@gmail.com>
2024-08-30 11:53:51 +0200

    Add some code for ship and player class

--- backend/bataille_navale.py
+++ backend/bataille_navale.py
@@ -8,17 +8,31 @@ class Board_game():
             line.append(' ')
         self.plateau.append(line)
         
+        
+        # self.cases = []
+        # for i in range(taille):
+        #     self.cases.append([])
+        # for j in range(taille):
+        #     self.cases[i].append("-")
+        
 
-class Navire():
-    def __init__(self, shipe_name, shipe_size ):
-        self.shipe_name = shipe_name
-        self.shipe_size = shipe_size
+class Ship():
+    def __init__(self, ship_name, ship_size ):
+        self.ship_name = ship_name
+        self.ship_size = ship_size
         
         # début de méthode pour la gestion des points de vie des navires
-        # def shipe_life():
-        #     shipe_life = shipe_size 
+        # def ship_life():
+        #     ship_life = ship_size 
         
         
 class Player():
-    def __init(self, name, #bateau, #grille ?):
-               
+    
+    player_ship_info = {"2" : "Porte-Avion ", "2" : "Torpilleur", "3" : "Croisseur", "3" : "Sous-Marin", "4" : "Porte-Avion"}
+    player_ship = [4,3,3,2,2,2]
+    def __init__(self, name):
+        self.ship_list = []
+        for i in self.player_ship:
+            self.ship_list.append(Ship("jkjkjkj",i))
+        
+    
